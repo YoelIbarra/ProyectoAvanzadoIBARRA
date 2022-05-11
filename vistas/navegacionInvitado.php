84aae7c31ff92a5a47eea44b2d7a484bf2764568
YoelIbarra <yoelibarra98@gmail.com>
2022-05-11 14:35:44 -0300

    Arranco de  0

--- vistas/navegacionInvitado.php
+++
@@ -1,7 +0,0 @@
-<nav class="navegacion-back">
-    <!--a class="navegacion-boton" href="contacto.php">Contacto</a>
-    <a class="navegacion-boton" href="comentario.php">Comentario</a!-->
-    <a class="navegacion-boton" href="index.php?ruta=invitado&&comision">Comisión</a>
-    <a class="navegacion-boton" href="index.php?ruta=invitado">Inscripción</a>
-    <a class="navegacion-boton" href="index.php">Login</a>
-</nav>
